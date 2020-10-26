468e17ff4ee5d8f5d911e29abf863239dd3d1584
jsy <jsy@callfire.com>
2020-10-26 03:59:59 -0700

    Test UI - able to list data from mock service.

--- src/app/components/todo/list/todo-list.component.ts
+++ src/app/components/todo/list/todo-list.component.ts
@@ -1,14 +1,21 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Todo} from '@src/app/models/todo.model';
+import {TodoApiService} from '@src/app/apis/todo/todo.api.service';
 
 @Component({
     selector: 'app-todo-list',
-    template: 'todo-list.component.html'
+    templateUrl: 'todo-list.component.html'
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit {
 
     public list: Todo[];
 
-    constructor() {
+    constructor(private todoApiService: TodoApiService) {
+    }
+
+    ngOnInit() {
+        this.todoApiService.getList()
+            .subscribe((list) => this.list = list);
+
     }
 }
